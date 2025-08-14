e037e4980c6fa2f22cd02c152c66a2a538029bc4
akmalfzy_ <113575805+5210411384@users.noreply.github.com>
2025-08-14 18:57:36 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b", // kasih '#' biar valid
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/line-clamp"), // biar bisa pakai line-clamp-3
+  ],
+};
